07ed09b043e8eb6c05a701ad26bf6af27d1c0483
Parker Lioa <joshf1558@gmail.com>
2020-05-04 16:31:34 +0800

    updated package.json, procfile, and helloworld.js PORT

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,13 +1,11 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url')
-
+const PORT = process.env.PORT || 5000
 
 http.createServer(function(req,res){
 	var q = url.parse(req.url,true);
 	var filename = "."+q.pathname;
-
-	
 
 	filename = filename+'.html';
 	if (filename=='./.html'){filename = './index.html'};
@@ -21,6 +19,6 @@ http.createServer(function(req,res){
 		res.write(data);
 		return res.end();
 	})
-}).listen(8080);
+}).listen(PORT);
 
 console.log("Server Listening on port 8080")
